3d35c14ebbdbfe8f6bfc92660cb4058c0c17c2d4
Carlos dos Anjos <carlos@goodtravelsoftware.com>
2019-03-21 15:41:24 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# api.carlosanjos.dev
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World from node.js");
+});
+
+app.listen(port, () => {
+    console.log(`available at http://localhost:${port}`);
+});
